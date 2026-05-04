b010f647717bda3e799763e0db8a1baa5fe7e78b
Elaine Lyons <elaine@lyons.app>
2026-05-04 17:47:03 -0400

    Initial Commit

---
+++ Meme Vault/Meme_VaultApp.swift
@@ -0,0 +1,17 @@
+//
+//  Meme_VaultApp.swift
+//  Meme Vault
+//
+//  Created by Elaine Lyons on 5/4/26.
+//
+
+import SwiftUI
+
+@main
+struct Meme_VaultApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
